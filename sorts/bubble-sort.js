4d0c9754dc399c2e352ab1cd8d48612f222263e1
oychao <ch.k.ouyang@gmail.com>
2017-09-27 23:40:00 +0800

    bubble-sort optimization

--- sorts/bubble-sort.js
+++ sorts/bubble-sort.js
@@ -2,10 +2,13 @@
  * bubble sort algorithm in javascript
  */
 
-module.exports = function(arr) {
+const defaultCompare = (a, b) => a - b;
+
+module.exports = (arr, compare) => {
+  compare = compare || defaultCompare;
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
-      arr[j] > arr[j + 1] ? [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]] : void 0;
+      compare(arr[j], arr[j + 1]) > 0 ? [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]] : void 0;
     }
   }
 };
--- test.js
+++ test.js
@@ -6,6 +6,7 @@ const bubbleSort = require('./sorts/bubble-sort');
 
 const arr = [1, 4, 2, 3, 6, 5, 7];
 
-bubbleSort(arr);
+// bubbleSort(arr);
+bubbleSort(arr, (a, b) => b - a);
 
 console.log(arr);
