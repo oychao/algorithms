45391e09060af344e549bb97ec902331366b9582
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-10-06 00:06:29 +0800

    binary search

--- src/basic/binary-search.js
+++ src/basic/binary-search.js
@@ -2,12 +2,12 @@
  * binary search
  */
 
-module.exports = (arr, target) => {
+module.exports = (arr, target, compare = (a, b) => a - b) => {
   let low = 0, high = arr.length - 1;
   while (low <= high) {
     let mid = Math.floor((low + high) / 2);
-    low = arr[mid] < target ? low + 1 : low;
-    high = arr[mid] >= target ? mid - 1 : high;
+    low = compare(arr[mid], target) < 0 ? low + 1 : low;
+    high = compare(arr[mid], target) >= 0 ? mid - 1 : high;
   }
   return low;
 };
