3f736ad634122e831daa2042d85308c4ef77f176
Ouyang Chao <ch.k.ouyang@gmail.com>
2018-04-26 10:38:01 +0800

    big-integer#plus

--- src/big-integer/plus.js
+++ src/big-integer/plus.js
@@ -3,7 +3,33 @@ const isLegalInteger = require('./is-legal-integer');
 module.exports = function(arr1, arr2) {
     isLegalInteger(arr1);
     isLegalInteger(arr2);
-    // TBD
+    arr1 = arr1.splice(0);
+    arr2 = arr2.splice(0);
+    let endFlag = false, sum = 0, i, v1, v2;
+    const result = [];
+    while(true) {
+        v1 = arr1.pop();
+        v2 = arr2.pop();
+        if(v1 === undefined || v2 === undefined) {
+            v1 = v1 || 0;
+            v2 = v2 || 0;
+            endFlag = true;
+        }
+        sum += v1 + v2;
+        if(sum > 9) {
+            result.unshift(sum - 10);
+            sum = 1;
+        } else {
+            result.unshift(sum);
+            sum = 0;
+        }
+        if(endFlag) {
+            break;
+        }
+    }
+    if(sum === 1) {
+        result.unshift(1);
+    }
+    return result;
 };
-
 
--- test/big-integer.spec.js
+++ test/big-integer.spec.js
@@ -7,15 +7,25 @@ const assert = require('assert');
 const bi = require('../src/big-integer');
 
 describe('big-integer', () => {
+  let arr1, arr2;
+  beforeEach(function() {
+    arr1 = [1, 9, 9];
+    arr2 = [4, 3];
+  });
   describe('#plus-one', () => {
-    it('should plus one correctly', () => {
-      assert(bi.plusOne([1, 9, 9]).toString() === '2,0,0');
-    });
     it('should throw an type error when integer array is in a illegal format', () => {
       assert.throws(() => bi.plusOne([1, 3, 4, 89, 55]), TypeError);
       assert.throws(() => bi.plusOne(), TypeError);
       assert.throws(() => bi.plusOne(['2', 1, 4]), TypeError);
       assert.throws(() => bi.plusOne('1,2,3'), TypeError);
+    });
+    it('should not change the origin number array', () => {
+      bi.plusOne(arr1);
+      assert(arr1.toString() === arr1.toString());
+    });
+    it('should plus one correctly', () => {
+      assert(bi.plusOne(arr1).join('') === '200');
+      assert(bi.plusOne(arr2).join('') === '44');
     });
   });
 
@@ -25,6 +35,14 @@ describe('big-integer', () => {
       assert.throws(() => bi.plus(), TypeError);
       assert.throws(() => bi.plus(['2', 1, 4]), TypeError);
       assert.throws(() => bi.plus('1,2,3'), TypeError);
+    });
+    it('should not change the origin number arrays', () => {
+      bi.plus(arr1, arr2);
+      assert(arr1.toString() === arr1.toString());
+      assert(arr2.toString() === arr2.toString());
+    });
+    it('should add a number to another number correctly', () => {
+      assert(bi.plus(arr1, arr2).join('') === '242');
     });
   });
 });
