6d54741fd84c91f6de8855a99483c4310d06243d
Ouyang Chao <ch.k.ouyang@gmail.com>
2018-05-11 00:08:04 +0800

    trie-tree

--- src/trie-tree/alibaba.js
+++ src/trie-tree/alibaba.js
@@ -1,23 +1,23 @@
+// question
+// https://stackoverflow.com/questions/50267324/alibaba-interview-print-a-sentence-with-min-spaces/
+
 const sentence = 'ilikealibaba';
 const words = ['i', 'like', 'ali', 'liba', 'baba', 'alibaba',];
 
 class TrieNode {
-    constructor() {}
+    constructor() { }
     set(a) {
         this[a] = this[a] || new TrieNode();
         return this[a];
     }
-    search(word) {
+    search(word, marks, depth = 1) {
         word = Array.isArray(word) ? word : word.split('');
         const a = word.shift();
         if (this[a]) {
-            if (this[a]._ && word.length === 0) {
-                return 1;
-            } else if (!this[a]._ && word.length === 0) {
-                return -100;
-            } else {
-                return 1 + this[a].search(word);
+            if (this[a]._) {
+                marks.push(depth);
             }
+            this[a].search(word, marks, depth + 1);
         } else {
             return 0;
         }
@@ -37,4 +37,49 @@ TrieNode.createTree = words => {
 };
 
 const t = TrieNode.createTree(words);
+
+function searchSentence(sentence) {
+    const marks = [];
+    t.search(sentence, marks);
+    const ret = {};
+    marks.map(mark => {
+        ret[mark] = searchSentence(sentence.slice(mark));
+    });
+    return ret;
+}
+
+const solutionTree = searchSentence(sentence);
+
+function deepTraverse(tree, sentence, targetLen = sentence.length) {
+    const stack = [];
+    const sum = () => stack.reduce((acc, mark) => acc + mark, 0);
+    const ret = [];
+    (function traverse(tree) {
+        const keys = Object.keys(tree);
+        keys.forEach(key => {
+            stack.push(+key);
+            if (sum() === targetLen) {
+                const result = [];
+                let tempStr = sentence;
+                stack.forEach(mark => {
+                    result.push(tempStr.slice(0, mark));
+                    tempStr = tempStr.slice(mark);
+                });
+                ret.push(result);
+            }
+            if(tree[key]) {
+                traverse(tree[key]);
+            }
+            stack.pop();
+        });
+    })(tree);
+    return ret;
+}
+
+const solutions = deepTraverse(solutionTree, sentence);
+
+solutions.sort((s1, s2) => s1.length - s2.length).forEach((s, i) => {
+    console.log(`${i + 1}. ${s.join(' ')} (${s.length - 1} spaces)`);
+});
+console.log('pick no.1');
 
