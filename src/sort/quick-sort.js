5f4fd7d00225d00b1750863ca7804c03ac0ff6ed
ouyang-chao <chao@ouyang.io>
2019-03-09 19:52:34 +0800

    [feature] another partition in quicksort

--- src/sort/quick-sort.js
+++ src/sort/quick-sort.js
@@ -15,9 +15,27 @@ const partition = (arr, from, to, compare) => {
 };
 
 const quickSort = (arr, from, to, compare) => {
-  const index = partition(arr, from, to, compare);
+  const index = partition2(arr, from, to, compare);
   from < index - 1 ? quickSort(arr, from, index - 1, compare) : void 0;
   index + 1 < to ? quickSort(arr, index + 1, to, compare) : void 0;
 };
 
-module.exports = (arr, compare = defaultCompare) => quickSort(arr, 0, arr.length - 1, compare);
+const partition2 = (arr, from, to, compare) => {
+  let i = from - 1, j = to + 1, pivotVal = arr[~~((from + to) / 2)];
+  while (true) {
+    do { i++; } while (compare(arr[i], pivotVal) < 0)
+    do { j--; } while (compare(arr[j], pivotVal) > 0)
+    if (i >= j) { return j; }
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+};
+
+const quickSort2 = (arr, from, to, compare) => {
+  if (from < to) {
+    const pivot = partition2(arr, from, to, compare);
+    quickSort2(arr, from, pivot, compare);
+    quickSort2(arr, pivot + 1, to, compare);
+  }
+};
+
+module.exports = (arr, compare = defaultCompare) => quickSort2(arr, 0, arr.length - 1, compare);
