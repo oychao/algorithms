ffc88e9ec2c9d468d07246fcdc0b9b3a943fc914
ouyang-chao <chao@ouyang.io>
2019-03-11 19:25:22 +0800

    [feature] binary search

--- src/basic/binary-search.js
+++ src/basic/binary-search.js
@@ -1,15 +1,46 @@
 /**
- * binary search
- */
+* binary search
+*/
 
 const defaultCompare = require('./default-compare');
 
-module.exports = (arr, target, compare = defaultCompare) => {
-  let low = 0, high = arr.length - 1;
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2);
-    low = compare(arr[mid], target) < 0 ? low + 1 : low;
-    high = compare(arr[mid], target) >= 0 ? mid - 1 : high;
+module.exports = {
+  precise(a, t, c = defaultCompare) {
+    let l = 0, r = a.length - 1, m;
+    while (l <= r) {
+      m = ~~((l + r) / 2);
+      if (c(a[m], t) < 0) {
+        l = m + 1;
+      } else if (c(a[m], t) > 0) {
+        r = m - 1;
+      } else {
+        return m;
+      }
+    }
+    return undefined;
+  },
+  leftMost(a, t, c = defaultCompare) {
+    let l = 0, r = a.length - 1, m;
+    while (l < r) {
+      m = ~~((l + r) / 2);
+      if (c(a[m], t) < 0) {
+        l = m + 1;
+      } else {
+        r = m;
+      }
+    }
+    return l;
+  },
+  rightMost(a, t, c = defaultCompare) {
+    let l = 0, r = a.length - 1, m;
+    while (l < r) {
+      m = ~~((l + r) / 2);
+      if (c(a[m], t) <= 0) {
+        l = m + 1;
+      } else {
+        r = m;
+      }
+    }
+    return l - 1;
   }
-  return low;
 };
--- test/basic.spec.js
+++ test/basic.spec.js
@@ -7,16 +7,41 @@ const assert = require('assert');
 const basic = require('../src/basic');
 
 describe('basic', () => {
-  let sortedArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  describe('#binary-search', () => {
-    it('find 5 return index 5', () => {
-      assert(basic.binarySearch(sortedArr, 5) === 5);
+  let sortedArr = [0, 1, 2, 5, 5, 6, 6, 7, 8, 9];
+
+  describe('#binary-search-precise', () => {
+    it('find 5 return index 4', () => {
+      assert(basic.binarySearch.precise(sortedArr, 5) === 4);
     });
     it('find 7 return index 7', () => {
-      assert(basic.binarySearch(sortedArr, 7) === 7);
+      assert(basic.binarySearch.precise(sortedArr, 7) === 7);
+    });
+    it('find 7.17 return undefined', () => {
+      assert(basic.binarySearch.precise(sortedArr, 7.17) === undefined);
+    });
+  });
+  
+  describe('#binary-search-leftmost', () => {
+    it('find 5 return index 5', () => {
+      assert(basic.binarySearch.leftMost(sortedArr, 5) === 3);
+    });
+    it('find 7 return index 7', () => {
+      assert(basic.binarySearch.leftMost(sortedArr, 7) === 7);
     });
     it('find 7.17 return index 8', () => {
-      assert(basic.binarySearch(sortedArr, 7.17) === 8);
+      assert(basic.binarySearch.leftMost(sortedArr, 7.17) === 8);
+    });
+  });
+  
+  describe('#binary-search-rightmost', () => {
+    it('find 5 return index 5', () => {
+      assert(basic.binarySearch.rightMost(sortedArr, 5) === 4);
+    });
+    it('find 7 return index 7', () => {
+      assert(basic.binarySearch.rightMost(sortedArr, 6) === 6);
+    });
+    it('find 7.17 return index 8', () => {
+      assert(basic.binarySearch.rightMost(sortedArr, 7) === 7);
     });
   });
 });
