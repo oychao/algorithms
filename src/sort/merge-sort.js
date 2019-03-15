805edf340f6baecd9b05743e0939f9c3b6c808b8
ouyang-chao <chao@ouyang.io>
2019-03-15 22:01:32 +0800

    [feature] better merge sort

--- src/sort/merge-sort.js
+++ src/sort/merge-sort.js
@@ -4,18 +4,46 @@
 
 const defaultCompare = require('../basic/default-compare');
 
-const merge = (left, right, compare) => {
-  const ret = [];
-  while(left.length && right.length) {
-    ret.push(compare(left[0], right[0]) < 0 ? left.shift() : right.shift());
+const m = (a, l, mid, r, t, c) => {
+  let p1 = l,
+    p2 = mid,
+    len1 = mid,
+    len2 = r,
+    i = l;
+  while (p1 < len1 && p2 < len2) {
+    if (c(a[p1], a[p2]) < 0) {
+      t[i++] = a[p1++];
+    } else {
+      t[i++] = a[p2++];
+    }
   }
-  return ret.concat(left).concat(right);
+  while (p1 < len1) {
+    t[i++] = a[p1++];
+  }
+  while (p2 < len2) {
+    t[i++] = a[p2++];
+  }
+  i = l;
+  while (i < r) {
+    a[i] = t[i];
+    i++;
+  }
 };
 
-module.exports = mergeSort = (arr, compare = defaultCompare) => {
-  if (arr.length < 2) {
-    return arr;
+const ms = (a, l, r, t, c) => {
+  if (r - l < 2) {
+    t[l] = a[l];
+    return;
   }
-  const mid = Math.floor(arr.length / 2);
-  return merge(mergeSort(arr.slice(0, mid), compare), mergeSort(arr.slice(mid), compare), compare);
+  const mid = ~~((r + l) / 2);
+  ms(a, l, mid, t, c);
+  ms(a, mid, r, t, c)
+  m(a, l, mid, r, t, c);
+};
+
+
+module.exports = mergeSort = (a, c = defaultCompare) => {
+  const t = [];
+  ms(a, 0, a.length, t, c);
+  return a;
 };
