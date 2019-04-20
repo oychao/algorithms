50598d3ee18ba5d8fa05764898d9fff5466b2a06
ouyang-chao <chao@ouyang.io>
2019-04-20 23:34:47 +0800

    [feature] leetcode problem 9

---
+++ leetcode/9.palindrome-number.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=9 lang=javascript
+ *
+ * [9] Palindrome Number
+ *
+ * https://leetcode.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (42.73%)
+ * Total Accepted:    554.3K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '121'
+ *
+ * Determine whether an integer is a palindrome. An integer is a palindrome
+ * when it reads the same backward as forward.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 121
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it
+ * becomes 121-. Therefore it is not a palindrome.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a
+ * palindrome.
+ * 
+ * 
+ * Follow up:
+ * 
+ * Coud you solve it without converting the integer to a string?
+ * 
+ */
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = function(x) {
+  if (x < 0) { return false }
+  if (x < 10) { return true }
+  const s = []
+  while (x > 0) {
+    s.push(x % 10)
+    x /= 10
+    x = ~~x
+  }
+  let i, half, len
+  for (i = 0, half = ~~(s.length / 2), len = s.length; i <= half; ++i) {
+    if (s[i] !== s[len - i - 1]) { return false }
+  }
+  return true
+};
+
