2c758219bf79f56067a660496d63878f61f26522
oychao <chao@ouyang.io>
2019-04-23 01:04:42 +0800

    [refactor] new code format

--- leetcode/21.merge-two-sorted-lists.js
+++ leetcode/21.merge-two-sorted-lists.js
@@ -2,24 +2,6 @@
  * @lc app=leetcode id=21 lang=javascript
  *
  * [21] Merge Two Sorted Lists
- *
- * https://leetcode.com/problems/merge-two-sorted-lists/description/
- *
- * algorithms
- * Easy (46.61%)
- * Total Accepted:    555K
- * Total Submissions: 1.2M
- * Testcase Example:  '[1,2,4]\n[1,3,4]'
- *
- * Merge two sorted linked lists and return it as a new list. The new list
- * should be made by splicing together the nodes of the first two lists.
- * 
- * Example:
- * 
- * Input: 1->2->4, 1->3->4
- * Output: 1->1->2->3->4->4
- * 
- * 
  */
 /**
  * Definition for singly-linked list.
@@ -33,7 +15,7 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
+const mergeTwoLists = function(l1, l2) {
   if (!l1) { return l2 }
   if (!l2) { return l1 }
   let pCur = { next: null }
@@ -56,5 +38,5 @@ var mergeTwoLists = function(l1, l2) {
     pCur.next = p2
   }
   return pHead.next
-};
+}
 
