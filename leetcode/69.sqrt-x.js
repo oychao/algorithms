744ae03187b8bb945816abcbfd0cf8ff8f4e7037
oychao <chao@ouyang.io>
2019-04-23 01:15:52 +0800

    [feature] leetcode problem 69

---
+++ leetcode/69.sqrt-x.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=69 lang=javascript
+ *
+ * [69] Sqrt(x)
+ */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const mySqrt = function(x) {
+  return ~~Math.sqrt(x)
+}
+
