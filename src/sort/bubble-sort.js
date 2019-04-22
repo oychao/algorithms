6375477370b0e3c54426671749c09c369276616b
oychao <chao@ouyang.io>
2019-04-22 18:45:25 +0800

    [feature] better bubble sort

--- src/sort/bubble-sort.js
+++ src/sort/bubble-sort.js
@@ -4,11 +4,13 @@
 
 const defaultCompare = require('../basic/default-compare');
 
-module.exports = (a, c = defaultCompare) => {
-  let i, j;
-  for (i = 0; i < a.length; i++) {
-    for (j = 0; j < a.length - i - 1; j++) {
+const bs = (a, c = defaultCompare) => {
+  let i, len, j;
+  for (i = 0, len = a.length; i < len; i++) {
+    for (j = 0; j < len - i - 1; j++) {
       c(a[j], a[j + 1]) > 0 ? [a[j], a[j + 1]] = [a[j + 1], a[j]] : void 0;
     }
   }
 };
+
+module.exports = bs;
