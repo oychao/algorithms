78ec713165cb69d3249ed46f6f63a3550e070fd0
oychao <chao@ouyang.io>
2020-08-15 23:00:25 +0800

    feat: 235

---
+++ leetcode/235.lowest-common-ancestor-of-a-binary-search-tree.go
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=235 lang=golang
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (49.96%)
+ * Likes:    2193
+ * Dislikes: 110
+ * Total Accepted:    415.7K
+ * Total Submissions: 832.1K
+ * Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of
+ * two given nodes in the BST.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
+ *
+ * Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant
+ * of itself according to the LCA definition.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the BST.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	cur := root
+	var low, high int
+	if p.Val < q.Val {
+		low = p.Val
+		high = q.Val
+	} else {
+		low = q.Val
+		high = p.Val
+	}
+
+	for low >= cur.Val || high <= cur.Val {
+		if high == cur.Val || low == cur.Val {
+			return cur
+		}
+
+		if high < cur.Val {
+			cur = cur.Left
+		} else if low > cur.Val {
+			cur = cur.Right
+		}
+	}
+	return cur
+}
+
+// @lc code=end
+
