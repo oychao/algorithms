926d504849b9b7662d0e377f6d1aea7eee2249de
oychao <chao@ouyang.io>
2020-08-15 22:23:00 +0800

    feat: 226

---
+++ leetcode/226.invert-binary-tree.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=226 lang=golang
+ *
+ * [226] Invert Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if nil == root {
+		return nil
+	}
+	temp := root.Left
+	root.Left = root.Right
+	root.Right = temp
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+// @lc code=end
